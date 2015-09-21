6886e65ecc414ae59c15a985fa4420239cb941f7
xiaoniu2014 <116445168@qq.com>
2015-09-21 09:30:29 +0800

    Initial Commit

---
+++ SpotlightTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SpotlightTest
+//
+//  Created by hw on 15/9/21.
+//  Copyright © 2015年 hongw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SpotlightTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SpotlightTest
+//
+//  Created by hw on 15/9/21.
+//  Copyright © 2015年 hongw. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
